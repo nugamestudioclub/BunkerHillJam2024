8fcefafb19024590b76f64d78dbb89b3edfed636
MarcoHampel <sebastianhampel1@gmail.com>
2024-03-17 15:56:29 -0400

    Added fix to Goomba Movement

--- Assets/Scripts/Enemies/GoombaMovement.cs
+++ Assets/Scripts/Enemies/GoombaMovement.cs
@@ -7,12 +7,18 @@ public class GoombaMovement : MonoBehaviour
     private Rigidbody rb;
     private bool goingLeft = false;
     private Vector3 direction = Vector3.left;
+    public Vector3 Direction { get { return direction; } }
     [SerializeField]
     private float speed = 5f;
+    [Tooltip("Max number of seconds without a turn at which point it forces a turn")]
+    [SerializeField]
+    private float maxTurnTimeInterval = 10f;
+    private float lastTurnTime = -1f;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        lastTurnTime = Time.time;
     }
 
     // Update is called once per frame
@@ -26,7 +32,10 @@ public class GoombaMovement : MonoBehaviour
         {
             direction = Vector3.right;
         }
-
+        if(Time.time-lastTurnTime > maxTurnTimeInterval)
+        {
+            direction = -direction;
+        }
     }
     
     private void FixedUpdate()
